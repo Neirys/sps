ca4d79dc0b6b05933ed92a2e4c6231ede00d7bd0
Yaman JAIOUCH <yjaiouch@gmail.com>
2016-10-04 22:08:19 +0200

    Initialize commit round II

---
+++ SPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SPS
+//
+//  Created by Yaman JAIOUCH on 04/10/2016.
+//  Copyright © 2016 Yaman JAIOUCH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
