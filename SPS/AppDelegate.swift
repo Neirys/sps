91b52de8ec9b08cba6b652ca1bfc9082408a3cda
Yaman JAIOUCH <yjaiouch@gmail.com>
2016-10-10 20:29:03 +0200

    Disable debug

--- SPS/AppDelegate.swift
+++ SPS/AppDelegate.swift
@@ -22,12 +22,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         
         #if DEBUG
-            let service = RandomProposalsStatusService()
-//            let service = ProposalsStatusService()
+//            let service = RandomProposalsStatusService()
+            let service = ProposalsStatusService()
             let synchronizer = ProposalsStatusSynchronizer(proposalsStatusService: service)
             let debugSynchronizer = PeriodicProposalsStatusSynchronizer(synchronizer: synchronizer, period: 10)
             
-            return ApplicationController(splitViewController: splitViewController, proposalsStatusSynchronizer: debugSynchronizer)
+            return ApplicationController(splitViewController: splitViewController, proposalsStatusSynchronizer: synchronizer)
         #else
             return ApplicationController(splitViewController: splitViewController, proposalsStatusService: ProposalsStatusService())
         #endif
